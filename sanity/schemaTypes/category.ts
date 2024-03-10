64b47398c96f5440fbb502754abfbcab9d301e5c
anuj-terkel <anuj@terkel.io>
2024-03-10 12:24:12 -0700

    Initial Commit : next + drizzle

--- sanity/schemaTypes/category.ts
+++ sanity/schemaTypes/category.ts
@@ -1,19 +1,19 @@
-import {defineField, defineType} from 'sanity'
+import {defineField, SchemaTypeDefinition} from 'sanity'
 
-export default defineType({
-  name: 'category',
-  title: 'Category',
-  type: 'document',
-  fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    }),
-  ],
-})
+export default {
+    name: 'category',
+    title: 'Category',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        }),
+        defineField({
+            name: 'description',
+            title: 'Description',
+            type: 'text',
+        }),
+    ],
+} as SchemaTypeDefinition
